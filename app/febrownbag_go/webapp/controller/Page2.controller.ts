ef228213bef58ea66e97783f0618e749ee6a4104
Godwinhur Odono <godwinhur.odono@accenture.com>
2025-02-24 10:17:13 +0000

    Acitivity4and5

--- app/febrownbag_go/webapp/controller/Page2.controller.ts
+++ app/febrownbag_go/webapp/controller/Page2.controller.ts
@@ -7,13 +7,23 @@ import UIComponent from "sap/ui/core/UIComponent";
  * @namespace febrownbaggo.controller
  */
 export default class Page2 extends Controller {
- 
     /*eslint-disable @typescript-eslint/no-empty-function*/
     public onInit(): void {
-       
+        // Your initialization logic (if needed)
     }
+    /**
+     * Handles the navigation when the back button is pressed
+     */
     public onNavBack(): void {
         (this.getOwnerComponent() as UIComponent).getRouter().navTo("RouteHome");
     }
-   
-}
+ 
+       /*eslint-disable @typescript-eslint/no-empty-function*/
+       public onInit(): void {
+     
+       }
+     
+         public onButtonPress(): void {
+           MessageToast.show("Good Day Goody");
+       }
+     }
